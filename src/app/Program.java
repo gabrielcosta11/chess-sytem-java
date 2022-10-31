baa51a7a8d2fe975d9b37a0c94c039f7e6821290
gabrielcosta11 <ganovo.costa@outlook.com>
2022-10-31 13:25:37 -0300

    create class: Position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,7 +1,13 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Position pos = new Position(5, 4);
+        System.out.println(pos);
+
+
     }
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,45 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+    
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    /**
+     * @return int return the row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * @param row the row to set
+     */
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    /**
+     * @return int return the column
+     */
+    public int getColumn() {
+        return column;
+    }
+
+    /**
+     * @param column the column to set
+     */
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+}
