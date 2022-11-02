2cf38cc0c0af2191cfa1e05a3574a33cfe0f4608
gabrielcosta11 <ganovo.costa@outlook.com>
2022-11-01 23:19:26 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,6 +33,7 @@ public class Board {
     }
 
     public Piece piece(Position position) {
+        // Returns the position of a piece
         return pieces[position.getRow()][position.getColumn()];
     }
 
@@ -45,6 +46,20 @@ public class Board {
 
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public Piece removePiece(Position position) {
+        if(!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+
+        if(piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
     }
 
     private boolean positionExists(int row, int column) {
