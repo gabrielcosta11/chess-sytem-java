3f014712b51b4ccf75009d34dbaa82f14c2611ec
gabrielcosta11 <ganovo.costa@outlook.com>
2022-11-14 14:46:30 -0300

    Implemmeting possible moves of King

--- src/app/UI.java
+++ src/app/UI.java
@@ -78,7 +78,7 @@ public class UI {
             System.out.print("-" + ANSI_RESET);
         } else {
             if(piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+                System.out.print(piece + ANSI_RESET);
             } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -67,8 +67,14 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
+        //White pieces
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+
+        //Black pieces
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK)); 
     }
 }
