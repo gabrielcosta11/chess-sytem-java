aca69cabdcd9232bee113d1e2f61ab9af163c278
gabrielcosta11 <ganovo.costa@outlook.com>
2022-11-14 21:40:17 -0300

    Swtching player each turn

--- src/app/Program.java
+++ src/app/Program.java
@@ -18,7 +18,7 @@ public class Program {
             try {
                 UI.clearScreen();
                 System.out.println();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
 
                 System.out.print("Source: ");
